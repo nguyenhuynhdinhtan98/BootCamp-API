348abf524b324360d600f1aeb615ac1d3bb0c148
Tân Nguyễn <nguyenhuynhdinhtan98@gmail.com>
2020-12-20 08:37:30 +0700

    Add schema course

---
+++ models/Course.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+var bootcamp = require("./Bootcamp");
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, "Please add title"],
+  },
+  description: { type: String, require: [true, "Please add description"] },
+  tuition: { type: String, require: [true, "Please add tuition"] },
+  miniumskill: {
+    type: String,
+    require: [true, "Please add miniumskill"],
+    enum: ["beginner", "intermediate", "advanced"],
+  },
+  scholarhipsAvailable: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: "bootcamp",
+    required: true,
+  },
+});
+module.exports = mongoose.model("Course", CourseSchema);
--- seeder.js
+++ seeder.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 //Config
 dotenv.config({ path: `./config/config.env` });
 mongoose.connect(process.env.URL, {
@@ -11,9 +12,11 @@ mongoose.connect(process.env.URL, {
   useUnifiedTopology: true,
 });
 const bootcamps = JSON.parse(fs.readFileSync("./data/bootcamps.json", "utf-8"));
+const course = JSON.parse(fs.readFileSync("./data/courses.json", "utf-8"));
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(course);
     process.exit();
   } catch (error) {
     console.log(error);
@@ -22,6 +25,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     process.exit();
   } catch (error) {
     console.log(error);
